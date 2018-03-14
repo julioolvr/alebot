723834e652183c346ec3e8385985820b15ea1513
Julio Olivera <julio.olvr@gmail.com>
2018-03-14 00:07:16 -0300

    Reply to swearing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Telegraf = require('telegraf');
 
 const log = require('./lib/logger');
 const extractors = require('./lib/extractors');
+const containsSwearing = require('./lib/swearing');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const secret = `bot/${Math.random()
@@ -17,19 +18,46 @@ if (process.env.NOW_URL) {
   bot.webhookReply = false;
 }
 
-bot.on('text', ctx => {
-  log.debug('Received text message', ctx.message);
+function runExtractors(ctx) {
+  log.debug('Received text message, looking for extractors', ctx.message);
+
   const extractor = extractors.find(extractor =>
     extractor.matches(ctx.message)
   );
 
   if (!extractor) return;
 
+  log.debug('Found extractor, processing...');
   extractor.process(ctx);
+}
+
+function runSwearing(ctx) {
+  const message = ctx.message;
+  log.debug('Received text message, checking user', message);
+
+  if (message.from.username !== process.env.RESPOND_TO_USERNAME) {
+    return;
+  }
+
+  const odds = Math.random();
+  log.debug('Message was from the selected user, odds are', odds);
+
+  if (containsSwearing(message.text) && odds < 0.4) {
+    log.debug('Replying with a photo');
+    ctx.replyWithPhoto(process.env.IMAGE_REPLY_URL, {
+      reply_to_message_id: message.message_id
+    });
+  }
+}
+
+bot.on('text', ctx => {
+  runExtractors(ctx);
+  runSwearing(ctx);
 });
 
 if (process.env.NOW_URL) {
   bot.startWebhook(`/${secret}`, null, port);
 } else {
+  log.debug('Starting bot with polling');
   bot.startPolling();
 }
